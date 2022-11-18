81a3a9da39964f7d0ca8a72e8465fc41f41b15f6
mshakir082 <mdshakirhussain298@gmail.com>
2022-11-18 21:23:49 +0530

    let's Start coding_Rect

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return (
+        <div>
+             <ol> Expense Item
+       <li>Food Rs 10</li>
+       <li>Petrol Rs 100</li>
+      <li>Movies Rs 200</li> 
+       </ol>
+        </div>
+    )
+}
+export default ExpenseItem;
