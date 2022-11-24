061101f03432294585030e4bf97a6c2af1847cb2
mshakir082 <mdshakirhussain298@gmail.com>
2022-11-24 12:55:20 +0530

    useState hook comes into code

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css';
 import Card from '../UI/Card'
@@ -6,14 +7,16 @@ const ExpenseItem = (props)=>{
     // const expenseDate=new Date();
     // const expenseTitle='Car Insurance';
     // const expensePrice =294.5;
+    const [title,setTitle] = useState(props.title)
     const clickHandler = () =>{
-        console.log('Clicked!!!');
+        setTitle('Clicked!!!')
+     
     }
     return (
         <Card key={props.id} className="expense-item" >
         <ExpenseDate date={props.date}/>
          <div className="expense-item__description">
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className="expense-item__price">${props.amount}</div>
         </div>
         <button onClick={clickHandler}>Delete Expense</button>
