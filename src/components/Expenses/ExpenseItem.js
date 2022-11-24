62086e4a31fbb265555d58ea4904b4288a63e780
mshakir082 <mdshakirhussain298@gmail.com>
2022-11-24 11:20:52 +0530

    click addEventLisiner

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,7 +6,9 @@ const ExpenseItem = (props)=>{
     // const expenseDate=new Date();
     // const expenseTitle='Car Insurance';
     // const expensePrice =294.5;
-    
+    const clickHandler = () =>{
+        console.log('Clicked!!!');
+    }
     return (
         <Card key={props.id} className="expense-item" >
         <ExpenseDate date={props.date}/>
@@ -14,6 +16,7 @@ const ExpenseItem = (props)=>{
             <h2>{props.title}</h2>
             <div className="expense-item__price">${props.amount}</div>
         </div>
+        <button onClick={clickHandler}>Delete Expense</button>
         </Card>
     )
 }
