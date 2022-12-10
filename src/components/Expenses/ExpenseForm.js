760e6569811e00f33ad1fa5c0f81bb1e90c97ab3
mshakir082 <mdshakirhussain298@gmail.com>
2022-12-10 16:09:18 +0530

    Challenges on Conditional Rendering

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -52,6 +52,9 @@ return (
             <input type='date' min="2019-01-01" max="2023-12-31" value={enteredDate} onChange={dateChangeHandler} />
         </div>
     </div>
+       <div>
+           <button className="form_button" type='button' onClick={props.onCancel}>Cancel</button>
+      </div>
     <div >
         <button className="form_button" type="submit" >Add Expense</button>
     </div>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm'
 const NewExpense = (props) => {
+  const [isEditing, setisEditing ] = useState(false)
     const SaveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -9,11 +10,22 @@ const NewExpense = (props) => {
     
     };
     props.onAddExpense(expenseData);
-    }
-    return <div className="new-expense">
-        <ExpenseForm/>
-        <ExpenseForm  onSaveExpenseData={SaveExpenseDataHandler} />
-    </div>
+    setisEditing(false)
+};
+const startEditingHandler = () => {
+  setisEditing(true);
+}
+
+const stopEditingHandler = () => {
+  setisEditing(false);
+}
     
+    return (
+    <div className="new-expense">
+         <ExpenseForm  onSaveExpenseData={SaveExpenseDataHandler} />
+        {!isEditing && <button onClick={startEditingHandler}>Add new expense</button>}
+         {isEditing && <ExpenseForm  onSaveExpenseData={SaveExpenseDataHandler} onCancel = {stopEditingHandler}/>}
+    </div>
+    );
 }
 export default NewExpense
